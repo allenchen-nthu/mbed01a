c25ef33f7d0b4d4c521e77bb18d0cbce7434792a
allenchen-nthu <a0211812@gmail.com>
2021-03-01 23:45:21 +0800

    add main.cpp

---
+++ mbed-os-test-program3/main.cpp
@@ -0,0 +1,27 @@
+#include "mbed.h"
+
+DigitalOut myLED(LED1);
+DigitalOut myLED2(LED3);
+
+//void Led(DigitalOut &ledpin);
+
+int main()
+{
+   myLED = 1;
+   myLED2 = 1;
+   while (true)
+   {
+      for(int i = 0; i < 4; ++i)
+      {
+         myLED = !myLED;
+         ThisThread::sleep_for(100ms);
+      }
+      for(int j = 0; j < 6; ++j)
+      {
+         myLED2 = !myLED2;
+         ThisThread::sleep_for(100ms);
+      }
+      //Led(myLED);
+      //Led(myLED2);
+   }
+}
